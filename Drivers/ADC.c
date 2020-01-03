62746c757c1d5b269d18eb7f87b56478a43478df
Mostafa-Farghly <mostafa.alaa.16897@gmail.com>
2020-01-03 15:16:29 +0200

    Add some of the configuration features to ADC header file. Start Implementing ADC initialization function in ADC source file.

--- Drivers/ADC.c
+++ Drivers/ADC.c
@@ -8,3 +8,18 @@
 #include "ADC.h"
 
 
+/* ADC Initialization function */
+void ADC_init(const ADC_ConfigType *Config_ptr)
+{
+	/* Gate clock to the ADC Module */
+	SIM->SCGC6 |= SIM_SCGC6_ADC0_MASK;
+	
+	/* Configure choosen port pin */
+	/* Gate clock to the choosen port pin */
+	/*TODO*/
+	/* Select analog for pin */
+	Config_ptr->Port->PCR[Config_ptr->ADC_Pin] &= ~PORT_PCR_MUX_MASK;
+	Config_ptr->Port->PCR[Config_ptr->ADC_Pin] |= PORT_PCR_MUX(0);
+	
+	/*TODO*/
+}
--- Drivers/ADC.h
+++ Drivers/ADC.h
@@ -10,6 +10,82 @@
 
 
 #include <MKL25Z4.H>
+#include "std_types.h"
+
+
+/*
+ * ADC Input Clock.
+ * ADCx_CFG1[ADICLK] value.
+ */
+typedef enum
+{
+	Bus_CLK = 0,		/* Bus Clock */
+	Bus_CLK_2,			/* Bus Clock divided by 2 */
+	ALT_Clk,			/* Alternate Clock */
+	Asynch_CLK			/* Asynchronous Clock */
+}Input_CLK;
+
+
+/*
+ * CLock Divide Select.
+ * ADCx_CFG1[ADIV] value.
+ */
+typedef enum
+{
+	Div_1 = 0,			/* Clock division factor = 1 */
+	Div_2,				/* Clock division factor = 2 */
+	Div_4,				/* Clock division factor = 4 */
+	Div_8				/* Clock division factor = 8 */
+}CLK_Divide;
+
+
+/*
+ * Select single ended or differential input.
+ * ADCx_SC1n[DIFF] value.
+ */
+typedef enum
+{
+	Single_Ended = 0,	/* Select Single-ended conversions and input channels */
+	Differential,		/* Select Differential conversions and input channels */
+}Input_Mode;
+
+
+/* ADC configurations struct */
+typedef struct
+{
+	Input_CLK CLK;			/* ADC Input Clock. Check enum CLK_Div for available values */
+	
+	CLK_Divide CLK_Div;		/* Clock Division Factor. Check enum CLK_Divide for available values */
+	
+	Input_Mode In_Mode;		/* Select single ended or differential input.
+								Check enum Input_Mode for available values */
+	
+	/*TODO*/
+	
+	uint8 ADC_Pin;			/* Pin number connected to ADC */
+	
+	/*
+	 * Port: Pointer of type PORT_Type defined in "MKL25Z4.H" header file
+	 * Choose a Port to connect to ADC
+	 * Assigned values should be one of 
+	 * (PORTB, PORTC, PORTD, or PORTE); defined in "MKL25Z4.H" header file
+	 */
+	PORT_Type *Port;
+	
+}ADC_ConfigType;
+
+
+/*
+ * Name: ADC_init
+ * Description: Initialize the ADC driver.
+ * @Return: void
+ * @Param: Pointer to a struct ADC_ConfigType
+ */
+void ADC_init(const ADC_ConfigType *Config_ptr);
 
 
 #endif /* ADC_H_ */
+
+
+
+
